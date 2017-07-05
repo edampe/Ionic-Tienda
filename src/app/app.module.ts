f730be5208077676b30e301f9f670496cc74e9e2
Edilberto Amado P <edampe01@gmail.com>
2017-07-05 16:50:44 -0500

    Clase 160

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { HttpModule } from '@angular/http';
+
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { CarritoService, 
+         ProductosService, 
+         UsuarioService } from '../providers/index.services';
+import { ImagenPipe } from '../pipes/imagen/imagen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ImagenPipe
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CarritoService,
+    ProductosService,
+    UsuarioService
+  ]
+})
+export class AppModule {}
---
+++ src/config/url.servicios.ts
@@ -0,0 +1,5 @@
+
+
+export const URL_SERVICIOS = "http://localhost:8080/rest/index.php"
+
+export const URL_IMAGENES = "http://localhost:8080/rest/public/img/productos/"
